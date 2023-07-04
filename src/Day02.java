72206ecf64089ad9052c23a54d5183d644176183
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-07-04 13:16:34 +0900

    .

--- src/Day02.java
+++ src/Day02.java
@@ -1,7 +1,8 @@
 package src;
+import java.lang.*;
 public class Day02 {
-    public static void main(String[] args)
-    {
-        System.out.println("23");
+    public static void main(String[] args) {
+        System.out.println("Hello world! 한글");
+        System.out.println(3.14159);
     }
 }
---
+++ src/Quiz01.java
@@ -0,0 +1,11 @@
+package src;
+public class Quiz01 {
+    public static void main(String[] args) {
+        System.out.println("=============================");
+        System.out.println("\t\t\t/)/)\t\t\t");
+        System.out.println("\t\t   (  ..)");
+        System.out.println("\t\t  (  >\u2661< )");
+        System.out.println("\t Have a Good Time.");
+        System.out.println("=============================");
+    }
+}
