223eefdfc380e96e926782f62f3feea50ed17d2e
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-07-04 17:37:51 +0900

    .

---
+++ Day02/Exam05.java
@@ -0,0 +1,11 @@
+package Day02;
+
+public class Exam05 {
+    public static void main(String[] args) {
+        int a = 10;
+        final int b = 20;
+        a = 20;
+        //b = 10; final 키워드를 사용해서 상수로 선언했기 때문에 재정의가 안됨.
+
+    }
+}
---
+++ Day02/Quiz03.java
@@ -0,0 +1,17 @@
+package Day02;
+
+public class Quiz03 {
+    public static void main(String[] args) {
+        int i = 100;
+        long l = 100L;
+        final byte ON = 1;
+
+        //모든 문자가 대문자인 경우 상수를 뜻한다는 약속이 있음.
+        /*
+        리터럴 값 : 100, 100L, 1
+        변수 : i, l
+        상수 : ON
+        예약어 : int , long, final, byte 등
+         */
+    }
+}
