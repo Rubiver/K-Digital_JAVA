722ac6ffff3583e8400515bb7c1b62d130a1ef75
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-07-12 15:41:22 +0900

    .

--- Day08/Exam02.java
+++ Day08/Exam02.java
@@ -2,9 +2,71 @@ package Day08;
 
 import it.sauronsoftware.ftp4j.FTPClient;
 
+import java.io.File;
+import java.text.Format;
+
 public class Exam02 {
     public static void main(String[] args) {
         //FTP, File Transfer Protocol
         FTPClient client = new FTPClient();
+        //client.connect("10.2.13.151");
+        //client.login("java", "1234");
+
+        try {
+            client.connect("10.2.3.11",21);
+        } catch (Exception e) {
+            System.out.println("서버 접속에 실패했습니다.");
+            e.printStackTrace(); //에러 수정을 위해 개발단계에선 반드시 작성
+                                 //배포 단계에선 제거.
+            System.exit(0);
+        }
+        try {
+            client.login("java", "1234");
+        }catch (Exception e){
+            System.out.println("로그인 실패");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        try {
+            client.changeDirectoryUp();
+            String files = client.currentDirectory();
+            System.out.println(files);
+        }catch (Exception e){
+            System.out.println("실패");
+        }
+        /*
+        File folder = new File("");
+        try {
+            System.out.println(client.list());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        File text = new File("C:/FTPHome/Download/DownloadComplete.mp3");
+        File mp3 = new File("C:/FTPHome/Download/DownloadComplete.mp3");
+        //이거 아니면 client.downloat("", new File("C:/FTPHome/Download/다운로드성공.txt"); 처럼 작성
+        //실행하고 싶은 코드
+        try
+        {
+            client.download("download.txt", text);
+            client.download("Adam Levine-1-Lost Stars.mp3", mp3);
+        }catch (Exception e)
+        {
+            System.out.println("다운로드 실패");
+        }*/
+
+        //접속 종료
+        try {
+            client.disconnect(true);
+            System.out.println("정상 종료됨.");
+        }catch (Exception e)
+        {
+            System.out.println("접속 종료도 실패;;");
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ Day08/Quiz01.java
@@ -0,0 +1,132 @@
+package Day08;
+
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPFile;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Quiz01 {
+    public static void main(String[] args) {
+        FTPClient client = new FTPClient();
+        Scanner sc = new Scanner(System.in);
+        int sel = 0;
+        System.out.println("===============");
+        System.out.println("1. Connect FTP Server ");
+        System.out.println("2. Exit Program ");
+        sel = Integer.parseInt(sc.nextLine());
+        switch (sel)
+        {
+            case 1:
+                System.out.println("Input IP :");
+                String ip = sc.nextLine();
+                System.out.println("Input Port :");
+                int port = Integer.parseInt(sc.nextLine());
+                try {
+                    System.out.println("Try to connect "+ip+" port :"+port);
+                    client.connect(ip,port);
+                    System.out.println("<< FTP Server is Connected >>");
+                }catch (Exception e)
+                {
+                    System.out.println("Connecting Server Failure..");
+                }
+                System.out.print("Input ID : ");
+                String id = sc.nextLine();
+                System.out.print("Input Password : ");
+                String passwd = sc.nextLine();
+                try{
+                    client.login(id,passwd);
+                    System.out.println("<< Login Complete >>");
+                }catch (Exception e)
+                {
+                    System.out.println("Login Failure..");
+                }
+                System.out.println("");
+                out : while(true)
+                {
+                    int menu = 0;
+                    System.out.println("1. Upload File ");
+                    System.out.println("2. Download File ");
+                    System.out.println("3. Delete File ");
+                    System.out.println("4. Disconnect FTP Server ");
+                    menu = Integer.parseInt(sc.nextLine());
+                    switch (menu){
+                        case 1:
+                            String filename = "";
+                            System.out.print("Input Upload file's absolute path : ");
+                            filename = sc.nextLine();
+                            File uploadFile = new File("/"+filename);
+                            try{
+                                client.upload(uploadFile);
+                                System.out.println("Upload Complete");
+                                break;
+                            }catch (Exception e)
+                            {
+                                System.out.println("Upload Failure..");
+                            }
+                            break;
+                        case 2:
+                            try {
+                                FTPFile[] list = client.list();
+                                for(int i=0; i<list.length; i++)
+                                {
+                                    System.out.println((i+1)+". "+list[i].getName());
+                                }
+                                System.out.print("Input download file name(number) : ");
+                                //String fileName = sc.nextLine();
+                                int selector = Integer.parseInt(sc.nextLine());
+                                String fileName = list[selector-1].getName();
+                                File downFile = new File("C:/FTPHome/Download/"+fileName);
+                                client.download(fileName,downFile);
+                                /*for(int i=0; i<list.length; i++)
+                                {
+                                    if(list[i].getName().equals(fileName))
+                                    {
+                                        System.out.println(list[i].getName()+" "+fileName);
+                                        client.download(fileName,downFile);
+                                        System.out.println("Download Complete");
+                                    }
+                                }*/
+                                break;
+                            }catch (Exception e) {
+                                System.out.println("Download Failure.");
+                            }
+                            break;
+                        case 3:
+                            try {
+                                FTPFile[] list = client.list();
+                                for(int i=0; i<list.length; i++)
+                                {
+                                    System.out.println(list[i].getName());
+                                }
+                                System.out.print("Input Delete file name : ");
+                                String fileName = sc.nextLine();
+                                client.deleteFile(fileName);
+                                break;
+                            }catch (Exception e) {
+                                System.out.println("Delete failure");
+                            }
+                            break;
+                        case 4:
+                            try {
+                                System.out.println("Disconnecting.....");
+                                client.disconnect(true);
+                                break out;
+                            }catch (Exception e) {
+                                System.out.println("Disconnect Failure");
+                            }
+                        default:
+                            System.out.println("Input error");
+                    }
+                }
+                break;
+            case 2:
+                System.out.println("Exit Program");
+                break;
+            default:
+                System.out.println("Input error");
+
+        }
+
+    }
+}
